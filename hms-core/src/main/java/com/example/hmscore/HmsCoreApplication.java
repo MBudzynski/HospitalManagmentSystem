f4fd8230c564a705c172b6f86f44147c30f704d4
MBudzynski <michalbu92@gmail.com>
2025-10-24 21:09:17 +0200

    Init hms-core

---
+++ hms-core/src/main/java/com/example/hmscore/HmsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.hmscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HmsCoreApplication.class, args);
+	}
+
+}
---
+++ hms-core/src/test/java/com/example/hmscore/HmsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hmscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmsCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
